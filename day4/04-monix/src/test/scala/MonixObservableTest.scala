ee014a8313a827b2b1d623b79703a3b1aa85db65
Christophe le cam <christophe.lecam.external@axa.com>
2022-07-18 18:10:50 +0200

    Add a test to confirm pb whith 12 // 23 makes 2 executed twice

--- day4/04-monix/src/test/scala/MonixObservableTest.scala
+++ day4/04-monix/src/test/scala/MonixObservableTest.scala
@@ -45,4 +45,33 @@ class MonixObservableTest extends MonixSpec {
     taskF.runToFuture
     taskF.runToFuture.foreach(println)
   }
+
+  "Observable.evalOnce with combination" must "evalOnce all the jobs" in {
+    println("Complex case")
+    val obs = Observable.evalOnce {
+      println("Effect2: should appear only once");
+      "Hello2!"
+    }
+    val obs1 = Observable.evalOnce {
+      println("effect: obs2")
+      "result:1"
+    }
+    val obs2 = Observable.evalOnce {
+      println("effect: obs2")
+      "result:2"
+    }
+    val obs3 = Observable.evalOnce {
+      println("effect: obs3")
+      "result:3"
+    }
+    val obsF1 = obs.flatMap(_ => obs1)
+    val obsF2 = obs.flatMap(_ => obs2)
+    val obsF3 = obs.flatMap(_ => obs3)
+    val obsF12Agg = obsF1.zip(obsF2).map(r => r._1 + " " + r._2)
+    val obsF23Agg = obsF2.zip(obsF3).map(r => r._1 + " " + r._2)
+    val obsFAgg = obsF12Agg.zip(obsF23Agg).map(r => r._1 + " " + r._2)
+    val taskF = obsFAgg.foreachL(println)
+    taskF.runToFuture
+    taskF.runToFuture.foreach(println)
+  }
 }
